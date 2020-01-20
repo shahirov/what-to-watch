ebc5e94bcd06f77f32261378832f855f7e5de030
shahirov <arafat.shakhirov@gmail.com>
2020-01-20 16:00:46 +0500

    render redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import { store } from './store'
 import { App } from './components/app'
 import { movies, movieOverview } from './mocks'
 
 ReactDOM.render(
-  <Router>
-    <App movies={movies} movieOverview={movieOverview} />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App movies={movies} movieOverview={movieOverview} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
