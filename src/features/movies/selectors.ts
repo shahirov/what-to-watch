85faa8b83a4643ee26865af1d99bdd3cf877c445
shahirov <arafat.shakhirov@gmail.com>
2020-01-20 21:38:12 +0500

    add memoized selector that selects genres from selected movies

--- src/features/movies/selectors.ts
+++ src/features/movies/selectors.ts
@@ -4,10 +4,21 @@ import { RootState } from '../../store'
 
 const selectMoviesState = (state: RootState) => state.movies
 
-const selectGenre = createSelector([selectMoviesState], movies => movies.genre)
+export const selectGenre = createSelector([selectMoviesState], movies => movies.genre)
 
 export const selectMovies = createSelector([selectMoviesState], movies => movies.movies)
 
 export const selectMoviesByGenre = createSelector([selectGenre, selectMovies], (genre, movies) =>
-  movies.filter(movie => movie.genre === genre)
+  genre === 'All genres' ? movies : movies.filter(movie => movie.genre === genre)
 )
+
+export const selectGenres = createSelector([selectMovies], movies => {
+  const genreSet: Set<string> = new Set()
+
+  movies.forEach(movie => genreSet.add(movie.genre))
+
+  const genres = Array.from(genreSet)
+  genres.sort().unshift('All genres')
+
+  return genres
+})
