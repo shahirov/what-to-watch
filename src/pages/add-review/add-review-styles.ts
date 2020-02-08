62a201237b1b396e70aae135be7d07836c094164
shahirov <arafat.shakhirov@gmail.com>
2020-02-08 20:10:24 +0500

    create add-review page component

---
+++ src/pages/add-review/add-review-styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const MovieCardHeader = styled.div`
+  position: relative;
+`
+
+export const Wrapper = styled.div`
+  padding-top: 62px;
+  width: 520px;
+  margin: 0 auto;
+`
---
+++ src/pages/add-review/add-review.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
+import { MovieCardHeader, Wrapper } from './add-review-styles'
+import { RootState } from '../../store'
+import { Logo } from '../../components/logo'
+import { Header } from '../../components/header'
+import { Movie } from '../../features/movies/slice'
+import { UserBlock } from '../../components/user-block'
+import { Breadcrumbs } from '../../components/breadcrumbs'
+import { MovieCardFull } from '../../components/movie-card-full'
+import { AddReviewForm } from '../../components/add-review-form'
+import { selectMovieById } from '../../features/movies/selectors'
+import { MovieCardPoster } from '../../components/movie-card-poster'
+import { MovieCardBackground } from '../../components/movie-card-background'
+
+export const AddReview = () => {
+  const { movieId } = useParams<{ movieId: string }>()
+
+  const movie = useSelector<RootState, Movie | undefined>(state =>
+    selectMovieById(state, parseInt(movieId))
+  )
+
+  if (!movie) {
+    return null
+  }
+
+  const { rating } = movie
+
+  return (
+    <MovieCardFull>
+      <MovieCardHeader>
+        <MovieCardBackground movie={movie} />
+        <Header userPage={false}>
+          <Logo color="light" />
+          <Breadcrumbs movie={movie} />
+          <UserBlock />
+        </Header>
+        <MovieCardPoster movie={movie} size="small" />
+      </MovieCardHeader>
+      <Wrapper>
+        <AddReviewForm defaultRating={rating} />
+      </Wrapper>
+    </MovieCardFull>
+  )
+}
